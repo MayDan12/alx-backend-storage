2b5b84bc75b8da257852daadba14e118ead9e0b5
MayDan12 <oluwamayowadan@gmail.com>
2024-05-09 19:47:51 +0000

    2-fan

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Script that ranks country origins
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
