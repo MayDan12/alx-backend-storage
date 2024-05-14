65febcbef6b4ef578b8cddb089398df96bf4dbb2
Oluwanimotele Mayowa Daniel <110539319+MayDan12@users.noreply.github.com>
2024-05-14 20:47:20 +0100

    Create 9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''The Task 9's module.
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return (result.inserted_id)
+  
